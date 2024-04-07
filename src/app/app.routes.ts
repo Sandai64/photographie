e8e92b88c9bc679b8d13897d5441eb8854e17a12
Erwan Egasse <erwan.egasse@outlook.com>
2024-04-07 14:14:13 +0200

    add correct routes for stocks & albums

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { AlbumsComponent } from './components/Albums/Albums.component';
 import { AppComponent } from './app.component';
+import { AlbumsComponent } from './components/Albums/Albums.component';
+import { StocksComponent } from './components/Stocks/Stocks.component';
+import { NotFoundComponent } from './components/NotFound/NotFound.component';
 
 export const routes: Routes = [
     { path: 'albums', component: AlbumsComponent },
-    { path: 'app', component: AppComponent, pathMatch: 'full' },
+    { path: 'stocks', component: StocksComponent },
+    { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
