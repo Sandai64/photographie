e4b6f5dae0fc33e04e0fa3e31646be17f93df4cd
Erwan Egasse <erwan.egasse@outlook.com>
2024-04-07 14:45:26 +0200

    draft initial Albums & Stocks templates

--- src/app/components/Albums/Albums.component.ts
+++ src/app/components/Albums/Albums.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AlbumSchema, AlbumsService } from '../../services/albums.service';
+import { StocksService } from '../../services/stocks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, RouterLink],
   templateUrl: `./Albums.component.html`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlbumsComponent { }
+export class AlbumsComponent
+{
+  StocksService = StocksService;
+
+  albums: Array<AlbumSchema> = AlbumsService.getAll();
+}
--- src/app/components/Stocks/Stocks.component.ts
+++ src/app/components/Stocks/Stocks.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StockSchema, StocksService } from '../../services/stocks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-stocks',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, RouterLink],
   templateUrl: './Stocks.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StocksComponent { }
+export class StocksComponent
+{
+  stocks: Array<StockSchema> = StocksService.getAll();
+}
