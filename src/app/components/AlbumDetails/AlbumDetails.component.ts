0340366c2910b0e82ad7ba89ed99da4fe549fb00
Erwan Egasse <erwan.egasse@outlook.com>
2024-04-07 16:23:25 +0200

    AlbumDetails tier 0 complete

--- src/app/components/AlbumDetails/AlbumDetails.component.ts
+++ src/app/components/AlbumDetails/AlbumDetails.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { AlbumsService } from '../../services/albums.service';
+import { LucideAngularModule } from 'lucide-angular';
+import { StocksService } from '../../services/stocks.service';
 
 @Component({
   selector: 'app-album-details',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, LucideAngularModule, RouterLink],
   templateUrl: './AlbumDetails.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlbumDetailsComponent { }
+export class AlbumDetailsComponent
+{
+  AlbumsService = AlbumsService;
+  StocksService = StocksService;
+
+  private activatedRoute = inject(ActivatedRoute);
+  album = AlbumsService.getFromID(this.activatedRoute.snapshot.params['albumID']);
+}
