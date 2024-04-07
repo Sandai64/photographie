22b356721a02182b2da69e919a352540412b5369
Erwan Egasse <erwan.egasse@outlook.com>
2024-04-07 13:53:14 +0200

    cmp: add AlbumsComponent & StocksComponent & routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { StocksService } from './services/stocks.service';
 import { AlbumsService } from './services/albums.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AlbumsComponent } from './components/Albums/Albums.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'albums', component: AlbumsComponent },
+    { path: 'app', component: AppComponent, pathMatch: 'full' },
+];
