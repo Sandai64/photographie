eb2fac12fbada539d100a70ae721cca747b6c2bd
Erwan Egasse <erwan.egasse@outlook.com>
2024-04-07 16:47:44 +0200

    Finish StockDetails T0

--- src/app/components/StockDetails/StockDetails.component.ts
+++ src/app/components/StockDetails/StockDetails.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { StocksService } from '../../services/stocks.service';
+import { AlbumSchema, AlbumsService } from '../../services/albums.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-stock-details',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, LucideAngularModule, RouterLink],
   templateUrl: './StockDetails.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StockDetailsComponent { }
+export class StockDetailsComponent
+{
+  AlbumsService = AlbumsService;
+  StocksService = StocksService;
+
+  private activatedRoute = inject(ActivatedRoute);
+  stock = StocksService.getFromID(this.activatedRoute.snapshot.params['stockID']);
+  albums : Array<AlbumSchema> | null = null;
+
+  ngOnInit() : void
+  {
+    if (!this.stock) { return; }
+    this.albums = AlbumsService.getAll().filter(e => e.stockID === this.stock!.id);
+  }
+}
